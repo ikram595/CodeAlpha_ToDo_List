f76d18fa5f950d959bc72ac5ddcd7ca73a02be1f
ikram595 <letaiefikram@gmail.com>
2024-02-15 11:44:37 +0100

    styling&added functionnalities

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
-function addTask() {}
-function countTotal() {}
-function countCompleted() {}
-function completeTask() {}
-function unCompleteTask() {}
-function countUncompleted() {}
-function priorityUp() {}
-function priorityDown() {}
+function addTask() {
+  let userInput = document.getElementById("task_input").value;
+  //console.log(userInput);
+  if (userInput !== "") {
+    let newTask = document.createElement("div");
+    let parentDiv = document.getElementById("tasks");
+    newTask.classList.add("task");
+    newTask.innerHTML = `<p><b>${userInput}</b></p>
+                    <div class="task-icons">
+                        <i class="fa-solid fa-check-double" style="color: #44ff00;" onclick=completeTask(this)></i>
+                        <i class="fa-solid fa-pen-to-square" style="color: #005eff;"></i>
+                        <i class="fa-solid fa-trash" style="color: #ff0000;"></i>
+                    </div>`;
+    parentDiv.appendChild(newTask);
+    document.getElementById("task_input").value = "";
+    countTasks();
+    displayPlaceHolderText();
+  }
+}
 function deleteTask() {}
+function editTask() {}
+function completeTask(icon) {
+  let subParentDiv = icon.parentElement;
+  let parentDiv = subParentDiv.parentElement;
+  parentDiv.classList.add("completed");
+}
+function displayPlaceHolderText() {
+  let tasksCount = document.querySelectorAll(".task").length;
+  let text = document.querySelector(".placeholder-message");
+  if (tasksCount !== 0) {
+    text.style.display = "none";
+  } else {
+    text.style.display = "block";
+  }
+}
+function countTasks() {
+  let displayCount = document.getElementById("tasks-count");
+  let totalTasksCount = document.querySelectorAll(".task").length;
+  let tasksCompletedCount = document.querySelectorAll(".completed").length;
+  let tasksUncompletedCount = totalTasksCount - tasksCompletedCount;
+
+  displayCount.textContent =
+    totalTasksCount +
+    " Total, " +
+    tasksCompletedCount +
+    " Completed, " +
+    tasksUncompletedCount +
+    " Pending.";
+  //console.log(totalTasksCount);
+}
